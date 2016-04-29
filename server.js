075865bd25ad1ef771906ed645461c7fd0caf853
wurflen <jonaskluebbers@gmail.com>
2016-04-29 18:29:33 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello\n');
+});
+
+server.listen(8000, function() {
+    console.log((new Date()) + ' Server is listening on port 8000');
+});
