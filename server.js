a3ce2a3b5d729603adaad93a10d30f2b2c1f0a22
wurflen <jonaskluebbers@gmail.com>
2016-05-15 18:47:37 -0400

    Test Postgres

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+var pg = require('pg');
 var WebSocketServer = require('ws').Server;
 var Operation = require('./client/operation').Operation;
 
@@ -8,6 +9,18 @@ var clients = {};
 var operations = [];
 var nextName = 0;
 var text = '';
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres. Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 var app = express();
 app.use(express.static(__dirname + '/client'));
